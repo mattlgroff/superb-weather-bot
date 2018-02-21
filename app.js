03d66dd86cba23dafd31e9becd96ebf1f07a4583
Matt Groff <mattlgroff@gmail.com>
2018-02-20 20:21:21 -0800

    DRY. Refactor. Added zip and location sharing weather.

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-require('dotenv').config();
-const owm = require('./controllers/openWeatherMap.js');
-const Telegraf = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+require('dotenv').config()
+const owm = require('./controllers/openWeatherMap.js')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 // Runs on start for new user.
 bot.start((ctx) => {
-  console.log('started:', ctx.from.id);
+  console.log('started:', ctx.from.id)
   return ctx.reply(`Welcome to Superb Weather Bot! 
-  See /help for usage. Source: Open Weather Map https://openweathermap.org/`);
+  See /help for usage. Source: Open Weather Map https://openweathermap.org/`)
 })
 
-// Input: City Name Output: Weather in that city.
+// Find weather by city name.
 bot.hears(/city/i, ctx => {
   // Remove '/city' from the city name string before sending.
-  let city = ctx.update.message.text.slice(6, ctx.update.message.text.length);
-  owm.byName(ctx, city);
-});
+  let city = ctx.update.message.text.slice(6, ctx.update.message.text.length)
+  owm.byName(ctx, city)
+})
+
+// Find weather by zip code
+bot.hears(/zip/i, ctx => {
+  // Remove '/city' from the city name string before sending.
+  let zip_and_country = ctx.update.message.text.slice(5, ctx.update.message.text.length)
+  let split = zip_and_country.split(' ')
+  let zip = split[0]
+  let country = split[1]
+
+  owm.byZip(ctx, zip, country)
+})
+
+// Find weather by location.
+bot.on('message', ctx => {
+  if(ctx.message.location){
+    owm.byLocation(ctx, ctx.message.location)
+  }
+})
 
 // Help command.
-bot.command('help', (ctx) => ctx.reply('Usage: /city San Diego'));
+bot.command('help', (ctx) => ctx.reply(`Usage: 
+  /city San Diego
+  /zip 92120
+  /zip 100-0001 JP
+  Share location on the Telegram app to get weather at your position.
+  `));
 
 // Be polite.
 bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
--- controllers/openWeatherMap.js
+++ controllers/openWeatherMap.js
@@ -1,95 +1,146 @@
-const request = require('request');
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
-const API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
+const request = require('request')
+const API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY
 
 module.exports = {
   byName: function(ctx, name){
-    request(`${API_URL}${name}&APPID=${API_KEY}`, (err, res, body) => {
-      // If there is an error.
+    const NAME_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
+    request(`${NAME_URL}${name}&APPID=${API_KEY}`, (err, res, body) => {
+      this.weatherRequest(ctx, err, res, body)
+    })
+  },
+  byLocation: function(ctx, location){
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&APPID=${API_KEY}`
+    request(url, (err, res, body) => {
+      this.weatherRequest(ctx, err, res, body)
+    })
+  },
+  byZip: function(ctx, zip, country){
+    let url
+    if(country === '' || country === undefined){
+      url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`
+    }
+    else{
+      country = country.toLowerCase()
+      url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&APPID=${API_KEY}`
+    }
+    request(url, (err, res, body) => {
+      this.weatherRequest(ctx, err, res, body)
+    })
+  },
+  kelvinToC: function(kelvin){
+    return Math.floor(kelvin - 273.15)
+  },
+  kelvinToF: function(kelvin){
+    return Math.floor(kelvin * (9/5) - 459.67)
+  },
+  weatherRequest: function(ctx, err, res, body){
+    // If there is an error requesting.
       if(err) {
-        console.error(err);
-        ctx.reply(err);
+        console.error(err)
+        ctx.reply(err)
+        return err
+      }
+
+      // If there is an erroronious status code response.
+      if(res.statusCode !== 200){
+        console.error(err)
+        ctx.reply(`I can't find the weather for that location.`)
+        return res.statusCode
       }
 
       // Just in case it's not a JSON.
-      const parsedBody = JSON.parse(body);
+      const parsedBody = JSON.parse(body)
 
       // If there is no city found or 404.
       if(parsedBody.cod === '404'){
-        ctx.reply(parsedBody.message);
-        return null;
+        ctx.reply(parsedBody.message)
+        return parsedBody.message
+      }
+      
+      // Weather Description and Emoji
+      let weatherDesc
+      let emoji
+      if(parsedBody.weather[0] && parsedBody.weather[0].description){
+        weatherDesc = parsedBody.weather[0].description
+        emoji = this.findEmoji(weatherDesc)
       }
 
-      // Let's take our data from the body.
-      let weather = parsedBody.weather[0].main;
-      let weatherDesc = parsedBody.weather[0].description;
-      let city = parsedBody.name;
-      let tempF = parsedBody.main.temp * (9/5) - 459.67; 
-          tempF = Math.floor(tempF);
+      // City Name
+      let city
+      if(parsedBody.name){
+        city = parsedBody.name
+      }
 
-      // Emojis are very important.
-      let emoji;
-      switch(weatherDesc){
+      // Temp F/C
+      let tempF
+      let tempC
+      if(parsedBody.main && parsedBody.main.temp){
+        tempF = this.kelvinToF(parsedBody.main.temp)
+        tempC = this.kelvinToC(parsedBody.main.temp)
+      }
+      
+      // The response.
+      let response = 
+        `The weather in ${city} is currently ${tempF}°F / ${tempC}°C and ${emoji} ${weatherDesc}.`
+  
+      ctx.reply(response)
+  },
+  findEmoji: function(desc){
+    switch(desc){
         case 'broken clouds':
-          emoji = '⛅';
-          break;
+          return '⛅'
+          break
 
         case 'scattered clouds':
-          emoji = '⛅';
-          break; 
+          return '⛅'
+          break
 
         case 'overcast clouds':
-          emoji = '☁';
-          break;  
+          return '☁'
+          break
 
         case 'light intensity drizzle':
-          emoji = '🌧';
-          break;  
+          return '🌧'
+          break
 
         case 'light intensity shower rain':
-          emoji = '🌧';
-          break;  
+          return '🌧'
+          break  
 
         case 'moderate rain':
-          emoji = '🌧';
-          break;  
+          return '🌧'
+          break  
 
         case 'fog':
-          emoji = '🌫';
-          break;  
+          return '🌫'
+          break  
 
         // There is no 'mist' emoji, only fog.
         case 'mist':
-          emoji = '🌫';
-          break;  
+          return '🌫'
+          break  
 
         case 'clear sky':
-          emoji = '☀'; 
-          break;   
+          return '☀' 
+          break   
 
         // TODO: Replace snow with real desc
         case 'snow':
-          emoji = '🌨';
-          break;  
+          return '🌨'
+          break  
 
         case 'light snow':
-          emoji = '🌨';
-          break;    
+          return '🌨'
+          break    
 
         // TODO: Replace thunderstorm with real desc
         case 'thunderstorm':
-          emoji = '🌩';
-          break;  
+          return '🌩'
+          break  
 
         default:
-          emoji = '☀'; 
-          break; 
+          return '☀' 
+          break 
       }
-
-      let response = 
-        `The weather in ${city} is currently ${tempF}°F and ${emoji} ${weatherDesc}.`;
-  
-      ctx.reply(response);
-    })
   }
 }
